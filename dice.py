7c2b663a6b334b8b2c15749daaba75e3a6c70bf5
william341 <wwokwilliam@gmail.com>
2019-10-02 14:59:03 -0700

    done

--- dice.py
+++ dice.py
@@ -1,3 +1,24 @@
-# Name:
-# Period
+# Name: William W
+# Period 6
 # Dice Rolling Simulator
+
+import random
+
+rollnums = [0, 0, 0, 0, 0, 0]
+
+# main code
+number = int(input("How many rolls? "))
+
+for i in range(number):
+    roll = random.randint(1, 6)
+    print(str(i + 1) + ": Rolled " + str(roll))
+    rollnums[roll - 1] += 1
+
+print("Total Rolls: " + str(number + 1))
+
+for i in range(6):
+    print(str(i + 1) + ": " + str(rollnums[i]))
+
+print("\nPercentages:")
+for i in range(6):
+    print(str(i + 1) + ": " + str(rollnums[i] / 10 * 100))
